8a1c28ba0169db606da51b7e755b5a10c8fa91e4
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-21 02:59:05 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 import requests
 user = input("Enter your username: ")
 while True:
-    parameter = input (user+": ")
-    r = requests.get('https://some-random-api.ml/chatbot?message='+parameter)
+    message = input (user+": ")
+    r = requests.get('https://some-random-api.ml/chatbot?message='+message)
     response_json  = r.json()
     bot_reply = response_json['response']
     print('Chatbot:',bot_reply)
